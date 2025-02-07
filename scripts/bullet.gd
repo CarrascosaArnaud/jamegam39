835307388437d263ed2b831f609bb43eb00ca79a
Yanis ADEKALOM <yanis971.60@gmail.com>
2025-02-07 21:15:49 +0100

    BULLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,11 +1,22 @@
-extends Area2D
+class_name Bullet extends Area2D
 
+static var bullet = preload("res://scenes/bullet.tscn")
+
+static func shoot(from: Vector2, toward: Vector2, at: float):
+	var b = bullet.instantiate()
+	b.global_position = from
+	b.velocity = from.direction_to(toward) * at
+	b.look_at(toward)
+	return b
+
+var velocity = Vector2.RIGHT
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+	#velocity = Vector2.from_angle(rotation_degrees) * 50
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	translate(velocity)
