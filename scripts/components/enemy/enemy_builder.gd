51cfa44294d62781fed7a933cc6d27ad8f0f2892
Quentin Yahia <quentin.yahia.pro@gmail.com>
2025-02-08 14:18:14 +0100

    Feat : enemy builder position :sparkles:

--- scripts/components/enemy/enemy_builder.gd
+++ scripts/components/enemy/enemy_builder.gd
@@ -4,29 +4,12 @@ extends Node2D
 @export var textures: Array[Texture2D]
 
 func _ready() -> void:
-	generate_random_enemy()
+	generate_random_enemy(Vector2(500, 300))
 
-func generate_random_enemy():	
+func generate_random_enemy(position: Vector2 = self.position):
 	var rng = RandomNumberGenerator.new()
 	var pattern = patterns[rng.randi_range(0, patterns.size() - 1)].instantiate() as Enemy
 	var indexTexture = rng.randi_range(0, textures.size() - 1)
-	print(indexTexture)
 	pattern.load_player(textures[indexTexture])
+	pattern.position = position
 	add_child(pattern)
-
-func get_all_tscn_from_folder_path(path: String, extensions: Array = []) -> Array[String]:
-	var tscns: Array[String] = []
-	var dir = DirAccess.open(path)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			print(file_name)
-			if !dir.current_is_dir() && extensions.any(func(extension): return extension == file_name.get_extension()):
-				var full_path = path.path_join(file_name)
-				tscns.append(full_path)
-			file_name = dir.get_next()
-	else:
-		print("An error occurred when trying to access the path.")
-		return []
-	return tscns
