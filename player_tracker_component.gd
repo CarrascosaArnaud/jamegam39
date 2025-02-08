19b0a5b308c6b059af339840fc79902cb0b4cebe
Quentin Yahia <quentin.yahia.pro@gmail.com>
2025-02-08 19:00:39 +0100

    Player tracker get his child instead a relative path

--- player_tracker_component.gd
+++ player_tracker_component.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _ready() -> void:
-	$"EnemySpawner".trackedNode = get_tree().current_scene.find_child("Player")
+	get_children()[0].trackedNode = get_tree().current_scene.find_child("Player")
