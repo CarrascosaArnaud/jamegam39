0dfe273ed7636e9b7b7ed7c08cf2185d0eb2d029
Quentin Yahia <quentin.yahia.pro@gmail.com>
2025-02-08 17:24:51 +0100

    Fix : circle pattern weapon fixed !! :beer:

--- scripts/components/pattern/follow_path_2D.gd
+++ scripts/components/pattern/follow_path_2D.gd
@@ -6,13 +6,17 @@ extends Enemy
 # speed in px per second
 @export var speed = 100
 
+var weaponInstance
+
 func load_player(texture: Texture2D, weapon: PackedScene):
 	spriteContainer = $PathFollow2D/Sprite
-	add_child(weapon.instantiate())
+	weaponInstance = weapon.instantiate()
+	add_child(weaponInstance)
 	super(texture, weapon)
 
 func _process(delta: float) -> void:
 	path_follow.progress += speed * delta;
+	weaponInstance.position = path_follow.position
 	if (path_follow.progress_ratio >= 1.0):
 		path.position += path.curve.get_point_position(path.curve.point_count - 1)
 		path_follow.progress_ratio = 0
