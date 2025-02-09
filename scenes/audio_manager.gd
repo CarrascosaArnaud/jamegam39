55d7449ccccaa1b678f1741021d797593ff88554
arnaud <carrascosarnaud@gmail.com>
2025-02-09 20:25:45 +0100

    parfait

--- scenes/audio_manager.gd
+++ scenes/audio_manager.gd
@@ -3,3 +3,4 @@ extends Node
 @onready var death_sound: AudioStreamPlayer = $DeathSound
 @onready var mob_death_sound: AudioStreamPlayer = $MobDeathSound
 @onready var music: AudioStreamPlayer = $Music
+@onready var damage: AudioStreamPlayer = $Damage
