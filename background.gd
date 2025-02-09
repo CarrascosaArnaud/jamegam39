2a6722fda1932a4d33253c6e6717871cee228409
Yanis <yanis971.60@gmail.com>
2025-02-09 18:31:44 +0100

    bisous bisous

--- background.gd
+++ background.gd
@@ -3,6 +3,7 @@ extends Sprite2D
 @export var backgroundPic: Array[Texture2D]
 @export var scrollSpeed: float = 20.0
 
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomBackground() 
 	var timer = Timer.new()
@@ -11,8 +12,11 @@ func _ready() -> void:
 	timer.timeout.connect(randomBackground)
 	add_child(timer)
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	offset.y += scrollSpeed * delta
+	
 
 func randomBackground():
 	if backgroundPic.size()>0:
